b162927a2560d060e02d738c51fd603c0eaa07d5
Brandon Yee <brandon.yee150@gmail.com>
2016-01-04 12:07:31 -1000

    Meeting 10
    Finished Meeting 10. Added 2 more dates.

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 "use strict";
-/* exported shion */
+/* global gus garry:true */
+/* eslint no-warning-comments: [1, { "terms": ["todo", "fix", "help"], "location": "anywhere" }] */
 
-var finishedMeetings = 8,
+var finishedMeetings = 10,
 	meetingDays = ["Past", "Past",
 		"Fri, Oct 16", "Mon, Oct 26",
 		"Wed, Nov 4", "Fri, Nov 13", "Mon, Nov 23",
-		"Thu, Dec 3", "Fri, Dec 11"
+		"Thu, Dec 3", "Fri, Dec 11",
+		"Mon, Jan 4", " Tue, Jan 12", "Thu, Jan 21 (?)"
 	],
-	shion;
+	i;
 
 /* http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array */
 function shuffle (array) {
@@ -38,87 +40,14 @@ function shuffle (array) {
 // }
 
 var people = ["", "Becca", "Florence", "Jason", "Kauai", "Sabine", "Brandon"],
-	colors = ["red", "orange", "yellow", "green", "blue", "violet"],
-	shion;
+	colors = ["red", "orange", "yellow", "green", "blue", "violet"];
 
 console.log(people);
-
-function generateCombos (randomness) {
-	var comb = [[]];
-	var i, j;
-	
-	for (i = 1; i < 7; i++) {
-		for (j = 1; j < 7; j++) {
-			if (i !== j && i < j) {
-				comb.push([people[i], people[j]]);
-			}
-		}
-	}
-	
-	console.log(comb);
-	comb.shift();
-	comb.shift();
-	comb.sort(function() {
-		return 0.5 - randomness;
-	});
-	console.log(comb);
-	
-	var meeting = 1,
-		$b;
-
-	$b = $("#left");
-	$b.empty();
-	$b.append("<h3>Meeting " + meeting + "</h3>");
-	$b.append("<span class='date'>" + meetingDays[meeting - 1] + "</span><br />");
-	$b.append("<span>" + "Becca" + "</span>, <span>" + "Florence" + "</span><br /><br />");
-	
-	for (var i = 0; i < comb.length; i++) {
-		meeting++;
-		if (i < comb.length / 2 - 1) {
-			$b = $("#left");
-		} else {
-			$b = $("#right");
-		}
-		$b.append("<h3>Meeting " + meeting + "</h3>");
-		if (meetingDays.length >= meeting) {
-			$b.append("<span class='date'>" + meetingDays[meeting - 1] + "</span><br />");
-		} else {
-			$b.append("<span class='date'>" + "Future" + "</span><br />");
-		}
-		$b.append("<span>" + comb[i][0] + "</span>, <span>" + comb[i][1] + "</span><br /><br />");
-	}
-
-	colorCode();
-
-	removeFinishedMeetings();
-
-	console.log("Program finished.");
-}
-
-var $f, $s, submitButton;
-
-$(document).ready(function () {
-	generateCombos(0.9);
-	// $f = $("form");
-	// $s = $f.find("input[type=submit]");
-
-
-	// $f.submit(function () {
-	// 	alert(submitButton.value);
-	// 	generateCombos(Number(submitButton.value));
-	// 	return false;
-	// })
-
-	// $s.click(function () {
-	// 	submitButton = this;
-	// })
-
-});
 
 function colorCode () {
 	console.log("Started colorCode function.");
 
-	for (var i = 0; i < people.length; i++) {
+	for (i = 0; i < people.length; i++) {
 		$("body")
 		.find("span:contains(" + people[i] + ")")
 		.each(function (j) {
@@ -134,11 +63,11 @@ function colorCode () {
 function removeFinishedMeetings () {
 	console.log("Started removeFinishedMeetings function.");
 
-	for (var i = 1; i <= finishedMeetings; i++) {
+	for (i = 1; i <= finishedMeetings; i++) {
 		$("body")
 		.find("h3:contains(" + i + ")")
-		.each(function () {
-			if ($(this).text().length == 8 + i.toString().length) {
+		.each(function removeFinishedMeetingsInner () {
+			if ($(this).text().length === 8 + i.toString().length) {
 				$(this).css("text-decoration", "line-through");
 			}
 		});
@@ -146,8 +75,64 @@ function removeFinishedMeetings () {
 	$("body")
 		.find("h3:contains(" + i + ")")
 		.each(function () {
-			if ($(this).text().length == 8 + i.toString().length) {
+			if ($(this).text().length === 8 + i.toString().length) {
 				$(this).css("font-weight", "bold");
 			}
 		});
 }
+
+function generateCombos (randomness) {
+	var comb = [[]];
+	var i, j;
+
+	for (i = 1; i < 7; i++) {
+		for (j = 1; j < 7; j++) {
+			if (i !== j && i < j) {
+				comb.push([people[i], people[j]]);
+			}
+		}
+	}
+
+	console.log(comb);
+	comb.shift();
+	comb.shift();
+	comb.sort(function generateCombosRandomAlgorithm () {
+		return 0.5 - randomness;
+	});
+	console.log(comb);
+
+	var meeting = 1,
+		$block;
+
+	$block = $("#left");
+	$block.empty();
+	$block.append("<h3>Meeting " + meeting + "</h3>");
+	$block.append("<span class='date'>" + meetingDays[meeting - 1] + "</span><br />");
+	$block.append("<span>" + "Becca" + "</span>, <span>" + "Florence" + "</span><br /><br />");
+
+	for (i = 0; i < comb.length; i++) {
+		meeting++;
+		if (i < comb.length / 2 - 1) {
+			$block = $("#left");
+		} else {
+			$block = $("#right");
+		}
+		$block.append("<h3>Meeting " + meeting + "</h3>");
+		if (meetingDays.length >= meeting) {
+			$block.append("<span class='date'>" + meetingDays[meeting - 1] + "</span><br />");
+		} else {
+			$block.append("<span class='date'>" + "Future" + "</span><br />");
+		}
+		$block.append("<span>" + comb[i][0] + "</span>, <span>" + comb[i][1] + "</span><br /><br />");
+	}
+
+	colorCode();
+
+	removeFinishedMeetings();
+
+	console.log("Program finished.");
+}
+
+$(document).ready(function documentReadyHappens () {
+	generateCombos(0.9);
+});
